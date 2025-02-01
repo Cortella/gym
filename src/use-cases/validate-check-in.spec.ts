fbd6e07d9c1eb36a181613e4bd4606ebc1819fba
Cortella <nicolau.cortella@gmail.com>
2025-02-01 19:07:00 -0300

    [feature] implments  inject dependence

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -11,11 +11,11 @@ describe("CheckIn Use Case", () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInUseCase(checkInsRepository);
 
-    // vi.useFakeTimers();
+    vi.useFakeTimers();
   });
 
   afterEach(() => {
-    // vi.useRealTimers();
+    vi.useRealTimers();
   });
 
   it("should be able to validate check-in", async () => {
@@ -36,5 +36,21 @@ describe("CheckIn Use Case", () => {
     await expect(() =>
       sut.execute({ checkInId: "inexistent-check-in-id" })
     ).rejects.toBeInstanceOf(ApiError);
+  });
+
+  it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const checkIn = await checkInsRepository.create({
+      user_id: "user-id",
+      gym_id: "gym-id",
+    });
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+    await expect(() =>
+      sut.execute({ checkInId: checkIn.id })).rejects.toBeInstanceOf(ApiError);
   });
 });
--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -2,9 +2,10 @@ import { STATUS_CODE } from "../utils/status-code";
 import { ApiError } from "../errors/ApiError";
 import { CheckInsRepository } from "../repositories/check-ins-repository";
 import { CheckIn } from "@prisma/client";
+import dayjs from "dayjs";
 
 interface ValidateCheckInUseCaseRequest {
-  checkInId: string
+  checkInId: string;
 }
 
 type ValidateCheckInUseCaseResponse = {
@@ -12,9 +13,7 @@ type ValidateCheckInUseCaseResponse = {
 };
 
 export class ValidateCheckInUseCase {
-  constructor(
-    private checkInsRepository: CheckInsRepository
-  ) {}
+  constructor(private checkInsRepository: CheckInsRepository) {}
 
   async execute({
     checkInId,
@@ -23,6 +22,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) throw new ApiError("Gym not found!", STATUS_CODE.NOT_FOUND);
 
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      "minutes"
+    );
+
+    if (distanceInMinutesFromCheckInCreation > 20)
+      throw new ApiError("Check-in too old!", STATUS_CODE.BAD_REQUEST);
+    
+    
     checkIn.validated_at = new Date();
 
     await this.checkInsRepository.save(checkIn);
